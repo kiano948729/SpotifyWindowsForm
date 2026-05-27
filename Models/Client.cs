a2317adba0ad9857cdb42f81045756d2dc1e9cae
kiano948729 <148862757+kiano948729@users.noreply.github.com>
2026-05-27 11:17:29 +0200

    Replace Form1 with MainMenuForm and add resources
    Remove Form1 files and introduce a new MainMenuForm (Designer + code) with sidebar, player bar (play/pause/stop), playlist cards and stubbed event handlers. Rename Form1.resx to MainMenuForm.resx and add project resources: play/pause/stop PNGs, Properties/Resources.resx and generated Resources.Designer.cs. Add a simple Models/Client class and update Program.Main to instantiate Client.Start() and launch MainMenuForm. Update csproj to include the embedded Resources.resx and generated designer file entries.

---
+++ Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyWindowsForm.Models
+{
+    public class Client
+    {
+        public string AppName { get; set; }
+
+        public Client()
+        {
+            AppName = "Spotify Windows Form";
+        }
+
+        public void Start()
+        {
+            
+        }
+    }
+}
